5a898a68ec4249ad088d98308ed849ea885adc5a
Ross MacDonald <rmacdonald@dal.ca>
2021-07-01 20:06:22 -0400

    Working cards and forum filtering

--- feed_reader/feed_reader/urls.py
+++ feed_reader/feed_reader/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from users.views import PostListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/<str:forum>/', PostListView.as_view(), name='forum_list')
 ]
--- feed_reader/users/views.py
+++ feed_reader/users/views.py
@@ -1,21 +1,17 @@
 from django.shortcuts import render, redirect 
-from django.contrib.auth import authenticate, login 
-from django.contrib.auth.forms import UserCreationForm 
 
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('/')
+from django.views.generic import ListView
+
+from users.models import Forum, Collection, Profile, \
+                         Post, Filter
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        forum = self.kwargs['forum']
+        print(forum) 
+        if forum == None:
+            return Post.objects.all()
         else:
-            return render(request, 'signup.html' {'form': form})
-    else:
-        form = UserCreationForm()
-        return render(request, 'signup.html', {'form': form})
+            return Post.objects.filter(forum__name=forum)
